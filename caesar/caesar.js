f542ab27c4ca41f98ff3af76f6151040cc81c645
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-14 00:24:17 +0200

    Finish caesar v1.1

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,30 +1,32 @@
 const caesar = function (str, num) {
     let asciiNum = [];
     let newStr = [];
+    // for large shift factors
     alphaPlus = num > 26 ? Math.ceil(num / 26) * 26 : 26;
     alphaMinus = num < -26 ? Math.floor(-num / 26) * 26 : 26;
 
     for (let i = 0; i < str.length; i++) {
-        if (str[i].match(/[A-Z]/)) {                         // ASCII between 65 and 90
+
+        if (str[i].match(/[A-Z]/)) {    // Uppercase: ASCII between 65 and 90
             asciiNum = str.charCodeAt(i) + num;
-            if (num > 0 && asciiNum >= 91) {
+            if (num > 0 && asciiNum > 90) {
                 asciiNum = asciiNum - alphaPlus;
-            } else if (num < 0 && asciiNum <= 64) {
+            } else if (num < 0 && asciiNum < 65) {
                 asciiNum = asciiNum + alphaMinus;
             }
-        } else if (str[i].match(/[a-z]/)) {                  // ASCII between 97 and 122
-            asciiNum = str.charCodeAt(i) + num;
-            if (num > 0 && asciiNum >= 123) {
-                asciiNum = asciiNum - alphaPlus;
-            } else if (num < 0 && asciiNum <= 96) {
-                asciiNum = asciiNum + alphaMinus;
-            }
-        } else {                                             // not a character, keep as it is
-            asciiNum = str[i];
-        }
-        if (typeof (asciiNum) == "number") {
             newStr.push(String.fromCharCode(asciiNum))
-        } else {
+
+        } else if (str[i].match(/[a-z]/)) {     // Lowercase: ASCII between 97 and 122
+            asciiNum = str.charCodeAt(i) + num;
+            if (num > 0 && asciiNum > 122) {
+                asciiNum = asciiNum - alphaPlus;
+            } else if (num < 0 && asciiNum < 97) {
+                asciiNum = asciiNum + alphaMinus;
+            }
+            newStr.push(String.fromCharCode(asciiNum))
+
+        } else {    // not a character, keep as it is
+            asciiNum = str[i];
             newStr.push(asciiNum);
         }
     }

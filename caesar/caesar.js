008dfc9f74d66a6a61a9e86f9ffc0f5c0b215d4f
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-14 16:41:34 +0200

    Finish v. 1.2.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -5,9 +5,6 @@ const caesar = function (str, num) {
 
     for (let i = 0; i < str.length; i++) {
 
-
-
-
         if (str[i].match(/[A-Z]/)) {    // Uppercase: ASCII between 65 and 90
             asciiNum = str.charCodeAt(i)
                 ;
@@ -19,18 +16,6 @@ const caesar = function (str, num) {
                 asciiNum += shift;
             }
             newStr.push(String.fromCharCode(asciiNum))
-
-            // } else if (clearText[i].match(/[a-z]/)) {     // Lowercase: ASCII between 97 and 122
-            //     asciiNum = clearText.charCodeAt(i);
-            //     if (shift > 0 && (asciiNum + shift) > 122) {
-            //         asciiNum = 64 + (shift - (90 - asciiNum));
-            //     } else if (shift < 0 && (asciiNum + shift) < 97) {
-            //         asciiNum = (91 + shift) + (asciiNum - 65);
-            //     } else {
-            //         asciiNum += shift;
-            //     }
-            //     newStr.push(String.fromCharCode(asciiNum))
-
 
         } else if (str[i].match(/[a-z]/)) {     // Lowercase: ASCII between 97 and 122
             asciiNum = str.charCodeAt(i);

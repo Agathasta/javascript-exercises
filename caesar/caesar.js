3ecc87345eeaaa362f748e8e92f4b8adbc3b8d12
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-14 16:37:48 +0200

    Finish v. 1.2

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,27 +1,45 @@
 const caesar = function (str, num) {
     let asciiNum = [];
     let newStr = [];
-    // for large shift factors
-    alphaPlus = num > 26 ? Math.ceil(num / 26) * 26 : 26;
-    alphaMinus = num < -26 ? Math.floor(-num / 26) * 26 : 26;
+    const shift = num % 26
 
     for (let i = 0; i < str.length; i++) {
 
+
+
+
         if (str[i].match(/[A-Z]/)) {    // Uppercase: ASCII between 65 and 90
-            asciiNum = str.charCodeAt(i) + num;
-            if (num > 0 && asciiNum > 90) {
-                asciiNum = asciiNum - alphaPlus;
-            } else if (num < 0 && asciiNum < 65) {
-                asciiNum = asciiNum + alphaMinus;
+            asciiNum = str.charCodeAt(i)
+                ;
+            if (shift > 0 && asciiNum + shift > 90) {
+                asciiNum = 64 + (shift - (90 - asciiNum));
+            } else if (shift < 0 && asciiNum + shift < 65) {
+                asciiNum = (91 + shift) + (asciiNum - 65);
+            } else {
+                asciiNum += shift;
             }
             newStr.push(String.fromCharCode(asciiNum))
 
+            // } else if (clearText[i].match(/[a-z]/)) {     // Lowercase: ASCII between 97 and 122
+            //     asciiNum = clearText.charCodeAt(i);
+            //     if (shift > 0 && (asciiNum + shift) > 122) {
+            //         asciiNum = 64 + (shift - (90 - asciiNum));
+            //     } else if (shift < 0 && (asciiNum + shift) < 97) {
+            //         asciiNum = (91 + shift) + (asciiNum - 65);
+            //     } else {
+            //         asciiNum += shift;
+            //     }
+            //     newStr.push(String.fromCharCode(asciiNum))
+
+
         } else if (str[i].match(/[a-z]/)) {     // Lowercase: ASCII between 97 and 122
-            asciiNum = str.charCodeAt(i) + num;
-            if (num > 0 && asciiNum > 122) {
-                asciiNum = asciiNum - alphaPlus;
-            } else if (num < 0 && asciiNum < 97) {
-                asciiNum = asciiNum + alphaMinus;
+            asciiNum = str.charCodeAt(i);
+            if (shift > 0 && asciiNum + shift > 122) {
+                asciiNum = 64 + (shift - (90 - asciiNum));
+            } else if (shift < 0 && asciiNum < 97) {
+                asciiNum = (91 + shift) + (asciiNum - 65);
+            } else {
+                asciiNum += shift;
             }
             newStr.push(String.fromCharCode(asciiNum))
 

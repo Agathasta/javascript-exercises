005b380f22ecbc7ea14f8f2db90172dad7572a84
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-15 10:44:14 +0200

    Add Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (max) {
+    if (max > 0) {
+        const fibo = [1, 1]
+        for (i = 2; i <= max; i++) {
+            fibo.push((fibo[i - 1]) + (fibo[i - 2]));
+        }
+        return fibo[max - 1];
+    } else {
+        return "OOPS";
+    }
 }
 
 module.exports = fibonacci

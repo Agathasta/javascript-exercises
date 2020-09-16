1e2bca4d5d42d3be2bfa7597d6ad6b5c484b06ee
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-16 18:35:31 +0200

    Add getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
 
+const getTheTitles = function () {
+    let titles = []
+    for (i = 0; i < books.length; i++) {
+        titles.push(books[i].title);
+    }
+    return titles;
 }
-
 module.exports = getTheTitles;

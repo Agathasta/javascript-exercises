8185e54baba29cdfd92b6af903b3d139a2e862ec
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-16 23:10:08 +0200

    Add findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,59 @@
-let findTheOldest = function() {
 
+
+let findTheOldest = function (pax) {
+    return pax.reduce((older, current) => {
+        const olderAge = getAge(older.yearOfDeath, older.yearOfBirth);
+        const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+        return olderAge > currentAge ? older : current;
+    })
 }
-
+function getAge(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 module.exports = findTheOldest
+
+
+
+
+
+// let findTheOldest = function (pax) {
+//     let alive;
+//     for (i = 0; i < pax.length; i++) {
+//         if (pax[i].hasOwnProperty("yearOfDeath")) {
+//             alive = false;
+//         } else {
+//             const date = new Date();
+//             pax[i].yearOfDeath = date.getFullYear();
+//             alive = true;
+//             i = pax.length;
+//         }
+//     }
+//     if (alive) {
+//         pax.sort(function (a, b) {
+//             const ageA = a.yearOfDeath - a.yearOfBirth;
+//             const ageB = b.yearOfDeath - b.yearOfBirth;
+//             return ageA > ageB ? -1 : 1;
+//         });
+//         return pax[0];
+//     } else {
+//         return "All are dead";
+//     }
+// }
+// module.exports = findTheOldest
+
+
+// // PASSES 1ST TEST!
+
+// let findTheOldest = function (pax) {
+//     pax.sort(function (a, b) {
+//         const ageA = a.yearOfDeath - a.yearOfBirth;
+//         const ageB = b.yearOfDeath - b.yearOfBirth;
+//         return ageA > ageB ? -1 : 1;
+//     });
+//     return pax[0];
+// }
+
+// module.exports = findTheOldest
